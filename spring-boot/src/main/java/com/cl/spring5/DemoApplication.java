3449add9260424c03b584784276d533e3e92debc
程丽斌 <chenglibin@souche.com>
2019-01-07 16:52:30 +0800

    spring-boot

---
+++ spring-boot/src/main/java/com/cl/spring5/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.cl.spring5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * https://www.cnblogs.com/ityouknow/p/5662753.html
+ * @author HP
+ *
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
